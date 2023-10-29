168a643e1ea993a8e303341aa7ff4e350ea4ceaa
suhasvrundavan <suhasvrundavan9@gmail.com>
2023-10-29 09:26:20 +0530

    Student Management System Commit

---
+++ src/main/java/com/studentmanagementsystem/service/impl/StudentServiceImpl.java
@@ -0,0 +1,43 @@
+package com.studentmanagementsystem.service.impl;
+
+import com.studentmanagementsystem.entity.Student;
+import com.studentmanagementsystem.repository.StudentRepository;
+import com.studentmanagementsystem.service.StudentService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+
+    private StudentRepository studentRepository;
+
+    public StudentServiceImpl(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    @Override
+    public Student saveStudent(Student student) {
+       return studentRepository.save(student);
+    }
+
+    @Override
+    public Student updateStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    @Override
+    public Student getStudentById(Long id) {
+        return studentRepository.findById(id).get();
+    }
+
+    @Override
+    public void deleteStudentById(Long id) {
+        studentRepository.deleteById(id);
+    }
+}
